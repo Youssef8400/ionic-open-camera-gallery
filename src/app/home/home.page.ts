9d78b446a85bf4ab952d180277094e499c409bb3
Youssef8400 <bachary73@gmail.com>
2024-08-19 14:35:41 +0200

    home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  photo: SafeResourceUrl | undefined;  // Property to hold the photo URL
 
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {}
 
+  async takePicture() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera
+      });
+
+      if (image && image.dataUrl) {
+        this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image.dataUrl);
+      } else {
+        console.error('No image data returned');
+      }
+    } catch (error) {
+      console.error('Error taking picture', error);
+    }
+  }
 }
